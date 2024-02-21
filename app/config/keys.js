f080e2b82b57ad532cdf34b04ce9947b243c5b93
dupplerEffect(cokkiesWar) <erikacevedo.pr@gmail.com>
2024-02-21 13:57:30 -0600

    cambio de usuario

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     secretKey: 'tu_clave_secreta_de_jwt',
-    mongoURI: 'mongodb+srv://eacevedo:AnnabelH123@core.jyaoc4l.mongodb.net/admin_albion_online'
+    mongoURI: 'mongodb+srv://sars_admin:V9aFDNl2860QGeZA@core.jyaoc4l.mongodb.net/admin_albion_online'
 };
